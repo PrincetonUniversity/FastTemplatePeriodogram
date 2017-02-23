d30be318ad48f76a023ea67ecccd0c8a1bf86d5e
Jake VanderPlas <jakevdp@gmail.com>
2017-02-23 09:29:29 -0800

    Test sgn and omega for M

--- pyftp/tests/test_ftp.py
+++ pyftp/tests/test_ftp.py
@@ -9,7 +9,7 @@ import pytest
 def M_slow(t, cos_phi, omega, cn, sn, sgn=1):
     cn, sn = np.broadcast_arrays(cn, sn)
     n = np.arange(1, len(sn) + 1)[:, None]
-    phi = np.arccos(cos_phi)
+    phi = sgn * np.arccos(cos_phi)
 
     return (np.dot(cn, np.cos(n * (omega * t - phi))) +
             np.dot(sn, np.sin(n * (omega * t - phi))))
@@ -17,14 +17,16 @@ def M_slow(t, cos_phi, omega, cn, sn, sgn=1):
 
 @pytest.mark.parametrize('nterms', [1, 2, 3])
 @pytest.mark.parametrize('cos_phi', [1, 0, -0.5, -1])
-def test_M_basic(nterms, cos_phi, omega=1.0, rseed=42):
+@pytest.mark.parametrize('sgn', [+1, -1])
+@pytest.mark.parametrize('omega', [0.9, 1.0, 1.1])
+def test_M_basic(nterms, cos_phi, sgn, omega, rseed=42):
     # test that M correctly creates a truncated Fourier series
     rand = np.random.RandomState(rseed)
     t = np.linspace(0, 3, 10)
     cn = rand.randn(nterms)
     sn = rand.randn(nterms)
 
-    M1 = M_slow(t, cos_phi, omega, cn, sn)
-    M2 = M(t, cos_phi, omega, cn, sn)
+    M1 = M_slow(t, cos_phi, omega, cn, sn, sgn)
+    M2 = M(t, cos_phi, omega, cn, sn, sgn)
 
     assert_allclose(M1, M2)
