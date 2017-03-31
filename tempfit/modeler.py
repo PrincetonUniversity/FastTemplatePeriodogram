4c5a0141ebabd1e71c4db69d06e51b4d6c473d2f
Jake VanderPlas <jakevdp@gmail.com>
2017-03-31 07:05:53 -0700

    ENH: add slow template modeler

--- tempfit/modeler.py
+++ tempfit/modeler.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 from functools import wraps
 
 import numpy as np
+from scipy import optimize
 
 from .utils import weights, ModelFitParams
 from .template import Template
@@ -155,6 +156,8 @@ class FastTemplateModeler(object):
         self : FastTemplateModeler
             Returns self
         """
+        # TODO: validate dy when it is float or None
+
         self.t = np.array(t)
         self.y = np.array(y)
         self.dy = np.array(dy)
@@ -499,3 +502,88 @@ class FastMultiTemplateModeler(FastTemplateModeler):
                      for template in self.templates ]
 
         return np.max(all_power, axis=0)
+
+
+class SlowTemplateModeler(object):
+    """Slow periodogram built from a template model.
+
+    When computing the periodogram, this performs a nonlinear optimization at
+    each frequency. This is used mainly for testing the faster method
+    available in FastTemplatePeriodogram
+
+    Parameters
+    ----------
+    template : Template object
+        callable object that returns the template value as a function of phase
+    """
+    # TODO: match the full API of FastTemplateModeler.
+    # Perhaps factor-out common routines into a base class?
+
+    def __init__(self, template=None):
+        self.template = template
+
+    def fit(self, t, y, dy=None):
+        """Fit periodogram to given data
+
+        Parameters
+        ----------
+        t : array_like
+            sequence of observation times
+        y : array_like
+            sequence of observations associated with times t
+        dy : float, array_like (optional)
+            error or sequence of observational errors associated with times t
+        """
+        self.t, self.y, self.dy = self._validate_inputs(t, y, dy)
+        return self
+
+    def _validate_inputs(self, t, y, dy):
+        if dy is None:
+            # TODO: handle dy = None case more efficiently
+            t, y, dy = np.broadcast_arrays(t, y, 1.0)
+        else:
+            t, y, dy = np.broadcast_arrays(t, y, dy)
+        if t.ndim != 1:
+            raise ValueError("Inputs (t, y, dy) must be 1-dimensional")
+        return t, y, dy
+
+    def _chi2_ref(self):
+        """Compute the reference chi-square"""
+        weights = self.dy ** -2
+        weights /= weights.sum()
+        ymean = np.dot(weights, self.y)
+        return np.sum((self.y - ymean) ** 2 / self.dy ** 2)
+
+    def _minimize_chi2_at_single_freq(self, freq):
+        # at each phase, use a linear model to find best [offset, amplitude]
+        # and then minimize this scalar function of phase
+        def chi2(phase):
+            shifted = self.template(self.t * freq - phase)
+            X = np.vstack([np.ones_like(shifted), shifted]).T
+            offset, amp = np.linalg.solve(np.dot(X.T, X),
+                                          np.dot(X.T, self.y))
+            y_model = offset + amp * shifted
+            return np.sum((self.y - y_model) ** 2 / self.dy ** 2)
+        return optimize.minimize_scalar(chi2)
+
+    def power(self, freq):
+        """Compute a template-based periodogram at the given frequencies
+
+        Parameters
+        ----------
+        freq : array_like
+            frequencies at which to evaluate the template periodogram
+
+        Returns
+        -------
+        power : np.ndarray
+            normalized power spectrum computed at the given frequencies
+        """
+        freq = np.asarray(freq)
+        results = list(map(self._minimize_chi2_at_single_freq, freq.flat))
+        failures = sum([not res.success for res in results])
+        if failures:
+            raise RuntimeError("{0}/{1} frequency values failed to converge"
+                               "".format(failures, freq.size))
+        chi2 = np.array([res.fun for res in results])
+        return np.reshape(1 - chi2 / self._chi2_ref(), freq.shape)
